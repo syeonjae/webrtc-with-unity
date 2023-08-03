abfdf13b796d60164d403148f43a15d5964a14d3
syeonjae <syeonjae@fakeeyes.co.kr>
2023-08-03 17:27:02 +0900

    소거 중...#5

--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -4,18 +4,15 @@ import { callapp } from "./callapp";
 const Test = () => {
   useEffect(() => {
     callapp(document.querySelector("#callapp1") as HTMLElement);
-    const callapp2 = document.querySelector("#callapp1").cloneNode(true);
-    callapp2.id = "callapp2";
-
-    callapp2.querySelector("h1").innerText = "Callapp2";
-    document.body.appendChild(callapp2);
-    callapp(callapp2);
+    // const callapp2 = document.querySelector("#callapp1").cloneNode(true);
+    // callapp2.id = "callapp2";
+    // document.body.appendChild(callapp2);
+    // callapp(callapp2);
   }, []);
   return (
     <div>
       {parse(`
             <div id="callapp1" class="grid-container">
-                <h1>Callapp1:</h1>
                 <input type="checkbox" name="audio" class="callapp_send_audio" checked autocomplete="off"> Audio
                 <input type="checkbox" name="video" class="callapp_send_video" checked autocomplete="off"> Video
                 <select name="video_devices" class="video_devices" >
